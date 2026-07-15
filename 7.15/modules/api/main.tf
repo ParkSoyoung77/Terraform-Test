85e7eafaa467778c3af8cd2122a7c22160a366c1
ParkSoyoung77 <fst704g@naver.com>
2026-07-15 15:21:56 +0900

    Feat: 테라폼 코드

--- 7.15/modules/api/main.tf
+++ 7.15/modules/api/main.tf
@@ -5,8 +5,17 @@ resource "aws_apigatewayv2_api" "std17_http_api" {
     ip_address_type = "ipv4"
 }
 
-# 통합 1: S3 정적 웹사이트 (HTTP 프록시)
-resource "aws_apigatewayv2_integration" "std17_s3_integration" {
+# 통합 1-A: S3 루트 (index.html)
+resource "aws_apigatewayv2_integration" "std17_s3_root_integration" {
+    api_id                 = aws_apigatewayv2_api.std17_http_api.id
+    integration_type       = "HTTP_PROXY"
+    integration_method     = "ANY"
+    integration_uri        = var.s3_website_endpoint
+    payload_format_version = "1.0"
+}
+
+# 통합 1-B: S3 나머지 경로 (network.html, compute.html, database.html 등)
+resource "aws_apigatewayv2_integration" "std17_s3_proxy_integration" {
     api_id                 = aws_apigatewayv2_api.std17_http_api.id
     integration_type       = "HTTP_PROXY"
     integration_method     = "ANY"
@@ -23,18 +32,18 @@ resource "aws_apigatewayv2_integration" "std17_lambda_integration" {
     payload_format_version = "2.0"
 }
 
-# 경로 1: GET / -> S3 (루트, index.html)
+# 경로 1: GET / -> S3 루트 통합
 resource "aws_apigatewayv2_route" "std17_root_route" {
     api_id    = aws_apigatewayv2_api.std17_http_api.id
     route_key = "GET /"
-    target    = "integrations/${aws_apigatewayv2_integration.std17_s3_integration.id}"
+    target    = "integrations/${aws_apigatewayv2_integration.std17_s3_root_integration.id}"
 }
 
-# 경로 1-1: GET /{proxy+} -> S3 (network.html, compute.html, database.html 등 나머지 전부)
+# 경로 1-1: GET /{proxy+} -> S3 프록시 통합
 resource "aws_apigatewayv2_route" "std17_proxy_route" {
     api_id    = aws_apigatewayv2_api.std17_http_api.id
     route_key = "GET /{proxy+}"
-    target    = "integrations/${aws_apigatewayv2_integration.std17_s3_integration.id}"
+    target    = "integrations/${aws_apigatewayv2_integration.std17_s3_proxy_integration.id}"
 }
 
 # 경로 2: GET /lambda -> Lambda
