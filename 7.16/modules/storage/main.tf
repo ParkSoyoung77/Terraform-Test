fda1287dc7025f0134d16034ccb85adaaae5516f
ParkSoyoung77 <fst704g@naver.com>
2026-07-16 10:41:03 +0900

    Feat: 테라폼 코드

--- 7.16/modules/storage/main.tf
+++ 7.16/modules/storage/main.tf
@@ -14,22 +14,12 @@ resource "aws_s3_bucket_versioning" "std17_s3_bucket_versioning" {
 # 퍼블릭 액세스 관리
 resource "aws_s3_bucket_public_access_block" "std17_s3_bucket-access" {
     bucket                  = aws_s3_bucket.std17_s3_bucket.id
-    block_public_acls       = false
-    ignore_public_acls      = false
-    block_public_policy     = false
-    restrict_public_buckets = false
+    block_public_acls       = true
+    ignore_public_acls      = true
+    block_public_policy     = true
+    restrict_public_buckets = true
 }
 
-# 정적 웹사이트 호스팅 기능 활성화
-resource "aws_s3_bucket_website_configuration" "std17_s3_bucket_web_config" {
-    bucket = aws_s3_bucket.std17_s3_bucket.id
-    index_document {
-        suffix = "index.html"
-    }
-    error_document {
-        key = "error.html"
-    }
-}
 
 # 버킷에 외부 접근에 대한 정책 정의
 resource "aws_s3_bucket_policy" "std17_s3_bucket_policy" {
