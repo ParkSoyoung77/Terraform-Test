528c259d27f05ac823d81dcbd84fff5ebd8d3930
ParkSoyoung77 <fst704g@naver.com>
2026-07-16 13:15:02 +0900

    Feat: 테라폼 코드

--- 7.16/modules/storage/outputs.tf
+++ 7.16/modules/storage/outputs.tf
@@ -10,14 +10,14 @@ output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.std17_cdn.domain_name
 }
 
-output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.std17_cdn.id
-}
+# output "cloudfront_distribution_id" {
+#   value = aws_cloudfront_distribution.std17_cdn.id
+# }
 
-output "cloudfront_oac_id" {
-  value = aws_cloudfront_origin_access_control.std17_s3_oac.id
-}
+# output "cloudfront_oac_id" {
+#   value = aws_cloudfront_origin_access_control.std17_s3_oac.id
+# }
 
-output "cloudfront_api_domain_name" {
-  value = aws_cloudfront_distribution.std17_cdn_api.domain_name
-}
+# output "cloudfront_api_domain_name" {
+#   value = aws_cloudfront_distribution.std17_cdn_api.domain_name
+# }
