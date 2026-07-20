25aecaec0f757562853b756f1a34044d1c26bef4
ParkSoyoung77 <fst704g@naver.com>
2026-07-20 09:36:27 +0900

    Feat: 테라폼 코드

--- pj1/modules/database/outputs.tf
+++ pj1/modules/database/outputs.tf
@@ -1,14 +1,14 @@
 output "db_instance_id" {
-    value = aws_db_instance.std17_mysql.id
+    value = aws_db_instance.std17_mysql_rds.id
 }
 
 output "db_endpoint" {
-    value = aws_db_instance.std17_mysql.endpoint
+    value = aws_db_instance.std17_mysql_rds.endpoint
 }
 
 output "db_address" {
     description = "포트가 붙지 않은 순수 호스트 주소 (Lambda DB_HOST용)"
-    value       = aws_db_instance.std17_mysql.address
+    value       = aws_db_instance.std17_mysql_rds.address
 }
 
 output "db_subnet_group_name" {
