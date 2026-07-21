ba19d2bc5dfc82a8f5df707e922b3f0502f54122
ParkSoyoung77 <fst704g@naver.com>
2026-07-21 16:42:03 +0900

    Feat: 테라폼 코드

--- pj2/modules/security2/main.tf
+++ pj2/modules/security2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "std17_db_sg" {
     name        = "std17-db-sg"
     vpc_id      = var.vpc_id
-    description = "RDS MySQL 접근 허용 (VPC1 Lambda -> TGW -> VPC2 RDS)"
+    description = "Allow RDS MySQL access from VPC1 Lambda via TGW"
 
     ingress {
         from_port   = 3306
