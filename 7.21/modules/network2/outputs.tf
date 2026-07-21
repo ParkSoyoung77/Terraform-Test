7330b78bdfdd3ad7860494556bd0ec946c164fb3
ParkSoyoung77 <fst704g@naver.com>
2026-07-21 09:59:19 +0900

    Feat: 테라폼 코드

---
+++ 7.21/modules/network2/outputs.tf
@@ -0,0 +1,27 @@
+output "vpc_id" {
+    value = aws_vpc.std17_vpc2.id
+}
+
+output "public_subnet_ids" {
+    value = aws_subnet.std17_public_subnets2[*].id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.std17_private_subnets2[*].id
+}
+
+output "igw_id" {
+    value = aws_internet_gateway.std17_vpc_igw2.id
+}
+
+output "nat_id" {
+    value = aws_nat_gateway.std17_nat2.id
+}
+
+output "public_rt_id" {
+    value = aws_route_table.std17_vpc2_public_rt.id
+}
+
+output "private_rt_id" {
+    value = aws_route_table.std17_vpc2_private_rt.id
+}
---
+++ 7.21/modules/network2/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr" {
+    description = "VPC CIDR 블록"
+    type        = string
+    default     = "10.10.0.0/16"
+}
+
+variable "azs" {
+    description = "사용할 가용영역 리스트 (public/private subnet count.index 순서와 매칭)"
+    type        = list(string)
+    default     = ["us-west-1a", "us-west-1c"]
+}
