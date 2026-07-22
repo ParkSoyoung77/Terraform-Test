e980c2957febc0624ce4d9b2eacdbaeafd1a0aaa
ParkSoyoung77 <fst704g@naver.com>
2026-07-22 10:47:58 +0900

    Feat: 테라폼 코드

--- 7.22/modules/compute/main.tf
+++ 7.22/modules/compute/main.tf
@@ -37,37 +37,37 @@ EOF
   tags = { Name = "std17-public-ec2" }
 }
 
-resource "aws_instance" "std17_amazon_ec2" {
+# resource "aws_instance" "std17_amazon_ec2" {
 
-  ami           = var.amazon_ami
-  instance_type = var.instance_type
+#   ami           = var.amazon_ami
+#   instance_type = var.instance_type
 
-  subnet_id                   = var.public_subnet_ids[0]
-  associate_public_ip_address = true
+#   subnet_id                   = var.public_subnet_ids[0]
+#   associate_public_ip_address = true
 
-  root_block_device {
-    volume_size           = 10
-    volume_type            = "gp3"
-    delete_on_termination  = true
-  }
+#   root_block_device {
+#     volume_size           = 10
+#     volume_type            = "gp3"
+#     delete_on_termination  = true
+#   }
 
-  key_name = var.key_name
+#   key_name = var.key_name
 
-  vpc_security_group_ids = [
-    var.security_group_id
-  ]
+#   vpc_security_group_ids = [
+#     var.security_group_id
+#   ]
 
-  # 유저데이터
-  user_data = <<-EOF
-#!/bin/bash
-dnf update -y
-dnf install -y nginx mysql-client
+#   # 유저데이터
+#   user_data = <<-EOF
+# #!/bin/bash
+# dnf update -y
+# dnf install -y nginx mariadb105
 
-systemctl enable nginx
-systemctl start nginx
-EOF
+# systemctl enable nginx
+# systemctl start nginx
+# EOF
 
-  user_data_replace_on_change = true
+#   user_data_replace_on_change = true
 
-  tags = { Name = "std17-amazon-ec2" }
-}
+#   tags = { Name = "std17-amazon-ec2" }
+# }
