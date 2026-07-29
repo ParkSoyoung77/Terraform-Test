3b6373f3d66a6c694708a0a1d70825bdd091928a
ParkSoyoung77 <fst704g@naver.com>
2026-07-29 09:36:49 +0900

    Feat: 테라폼 코드

---
+++ 7.29/modules/compute/outputs.tf
@@ -0,0 +1,3 @@
+output "public_ec2_id" {
+    value = aws_instance.std17_public_ec2.id
+}
---
+++ 7.29/modules/compute/variables.tf
@@ -0,0 +1,32 @@
+variable "vpc_id" {
+    description = "S3 엔드포인트가 속할 VPC ID"
+    type        = string
+}
+
+variable "public_subnet_ids" {
+    description = "프라이빗 서브넷 ID 리스트"
+    type        = list(string)
+}
+
+variable "security_group_id" {
+    description = "EC2에 적용할 보안그룹 ID"
+    type        = string
+}
+
+variable "key_name" {
+    description = "EC2 키페어 이름"
+    type        = string
+    default     = "std17-key"
+}
+
+variable "instance_type" {
+    description = "EC2 인스턴스 타입"
+    type        = string
+    default     = "t3.micro"
+}
+
+variable "instance_ami" {
+    description = "EC2 인스턴스 소스 AMI (Ubuntu)"
+    type        = string
+    default     = "ami-0086ee55a149bd32e"
+}
