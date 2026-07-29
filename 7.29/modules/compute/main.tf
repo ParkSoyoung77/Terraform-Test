2eb416c329ce70724924d578a0d3dd69c1a80269
ParkSoyoung77 <fst704g@naver.com>
2026-07-29 10:39:38 +0900

    Feat: 테라폼 코드

--- 7.29/modules/compute/main.tf
+++ 7.29/modules/compute/main.tf
@@ -73,7 +73,7 @@ resource "aws_volume_attachment" "std17_extra_volume_attach" {
 # S3 Gateway VPC 엔드포인트
 resource "aws_vpc_endpoint" "std17_gw_endpoint" {
   vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.us-west-1.s3"
+  service_name      = "com.amazonaws.ap-northeast-3.s3"
   vpc_endpoint_type = "Gateway"
 
   route_table_ids = var.route_table_ids
