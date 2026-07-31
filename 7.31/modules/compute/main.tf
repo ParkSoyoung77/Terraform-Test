aa45247286b4afafe18f1037e3c23e138659253b
ParkSoyoung77 <fst704g@naver.com>
2026-07-31 09:40:21 +0900

    Feat: 테라폼 코드

---
+++ 7.31/modules/compute/main.tf
@@ -0,0 +1,82 @@
+resource "aws_instance" "std17_public_ec2" {
+
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+
+  subnet_id                   = var.public_subnet_ids[0]
+  associate_public_ip_address = true
+  iam_instance_profile        = var.iam_instance_profile
+
+  instance_market_options {
+    market_type = "spot"
+
+    spot_options {
+      spot_instance_type            = "one-time"
+      instance_interruption_behavior = "terminate"
+    }
+  }
+
+  root_block_device {
+    volume_size           = 10
+    volume_type            = "gp3"
+    delete_on_termination  = true
+  }
+
+  key_name = var.key_name
+
+  vpc_security_group_ids = [
+    var.security_group_id
+  ]
+
+  user_data = file("${path.module}/scripts/user_data.sh")
+  user_data_replace_on_change = true
+
+  tags = { Name = "std17-public-ec2" }
+}
+
+# ==================================================================
+# 추가 EBS 볼륨 (8GB)
+# ==================================================================
+resource "aws_ebs_volume" "std17_extra_volume" {
+  availability_zone = aws_instance.std17_public_ec2.availability_zone
+  size               = 8
+  type               = "gp3"
+
+  tags = { Name = "std17-public-ec2-extra" }
+}
+
+resource "aws_volume_attachment" "std17_extra_volume_attach" {
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.std17_extra_volume.id
+  instance_id = aws_instance.std17_public_ec2.id
+}
+
+# ==================================================================
+# 엔드포인트
+# ==================================================================
+
+# S3 Gateway VPC 엔드포인트
+resource "aws_vpc_endpoint" "std17_gw_endpoint" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.ap-northeast-3.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = var.route_table_ids
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "Statement1"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "*"
+        Resource  = "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "std17-gw-endpoint"
+  }
+}
