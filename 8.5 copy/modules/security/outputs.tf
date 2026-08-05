db59389dbc79e4a2cbe52f81fc0592cc6a57902a
ParkSoyoung77 <fst704g@naver.com>
2026-08-05 09:38:49 +0900

    Feat: README.md

--- 8.5 copy/modules/security/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "test_sg_id" {
-    value = aws_security_group.std17_test_sg.id
-}
-
-output "db_sg_id" {
-    description = "RDS용 보안그룹 ID"
-    value       = aws_security_group.std17_db_sg.id
-}
--- 8.5 copy/modules/security/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "vpc_id" {
-  description = "보안그룹을 생성할 VPC ID"
-  type        = string
-}
