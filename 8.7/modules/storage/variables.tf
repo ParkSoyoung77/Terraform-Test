5748e22b8c5a5ac5b9232016a7f672594723a772
ParkSoyoung77 <fst704g@naver.com>
2026-08-07 09:35:40 +0900

    Feat: 테라폼 코드

---
+++ 8.7/modules/storage/variables.tf
@@ -0,0 +1,17 @@
+variable "bucket_name" {
+    description = "S3 버킷 이름"
+    type        = string
+    default     = "std17-ex-bucket"
+}
+
+variable "mysql_html_path" {
+    description = "업로드할 mysql.html 경로"
+    type        = string
+    default     = ""
+}
+
+variable "backup_bucket_name" {
+    description = "S3 버킷 이름"
+    type        = string
+    default     = "std17-backup"
+}
