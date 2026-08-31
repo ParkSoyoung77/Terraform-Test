ed5d52510de932972f3f80aedb531ba355f7d1d0
ParkSoyoung77 <fst704g@naver.com>
2026-08-31 17:22:57 +0900

    Feat: 테라폼 코드

--- 8.28/modules/eks/main.tf
+++ 8.28/modules/eks/main.tf
@@ -80,6 +80,13 @@ resource "aws_security_group" "std17_eks_cluster_sg" {
     vpc_id      = var.vpc_id
     description = "EKS control plane to node communication SG"
 
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     tags = { Name = "std17-eks-cluster-sg" }
 }
 
@@ -88,27 +95,14 @@ resource "aws_security_group" "std17_eks_node_sg" {
     vpc_id      = var.vpc_id
     description = "EKS worker node SG"
 
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     tags = { Name = "std17-eks-node-sg" }
-}
-
-# 클러스터 SG 전체 아웃바운드 허용
-resource "aws_vpc_security_group_egress_rule" "std17_cluster_egress_all" {
-    security_group_id = aws_security_group.std17_eks_cluster_sg.id
-    ip_protocol        = "-1"
-    cidr_ipv4          = "0.0.0.0/0"
-    description         = "cluster SG all outbound"
-
-    depends_on = [aws_security_group.std17_eks_cluster_sg]
-}
-
-# 노드 SG 전체 아웃바운드 허용
-resource "aws_vpc_security_group_egress_rule" "std17_node_egress_all" {
-    security_group_id = aws_security_group.std17_eks_node_sg.id
-    ip_protocol        = "-1"
-    cidr_ipv4          = "0.0.0.0/0"
-    description         = "node SG all outbound"
-
-    depends_on = [aws_security_group.std17_eks_node_sg]
 }
 
 # 노드 -> 클러스터 API 서버 (443) : kubelet이 컨트롤플레인에 join/통신하기 위해 반드시 필요
